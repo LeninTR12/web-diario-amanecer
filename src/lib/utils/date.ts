b7e5dbbaf0df9fbc477e119e1938258480b53ef2
LeninTR12 <lenintr12@gmail.com>
2025-04-03 14:15:29 -0500

    wip: fixed date in home page posts

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,9 +1,11 @@
-import { formatDistanceToNow, parseISO, format } from "date-fns";
+import { formatDistanceToNow, parseISO, format, formatDistanceToNowStrict } from "date-fns";
 import { es } from "date-fns/locale";
 
 export const getDateDistance = (date: string) =>
-  formatDistanceToNow(parseISO(date), {
-    locale: es, addSuffix: true
+  formatDistanceToNowStrict(parseISO(date), {
+    locale: es, 
+    addSuffix: true,
+    roundingMethod: "floor"
   });
 
   
@@ -14,8 +16,24 @@ export const formatDate = (
 ) => {
   const parseDate = parseISO(date);
   if (formatDate === "short") {
-    return format(parseDate, "MMMM dd, yyyy zz");
+    return format(parseDate, "MMMM dd, yyyy", {locale: es});
   }
 
-  return format(parseDate, "EEEE, MMMM d, yyyy h:mm a zz");
+  return format(parseDate, "EEEE, d 'de' MMMM , yyyy hh:mm aaa  ", {locale: es});
 };
+
+export const formatDateTime = (isoDate: string) => {
+  const date = new Date(isoDate); 
+  const isToday = new Date();
+  const limitDays = 6*24*60*60*1000;
+
+  const isDistance = (isToday.getTime() - date.getTime()) < (limitDays);
+
+  if(isDistance){
+    return getDateDistance(isoDate);
+  }
+    
+  return formatDate(isoDate, "short");
+  
+
+}
